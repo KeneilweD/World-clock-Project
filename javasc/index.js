54ed6f7600d15396bc9307fb29dd26f1ed1eda44
KeneilweD <keneilwed.mohohlo@gmail.com>
2025-05-04 15:44:53 +0200

    insert javascript for two cities to display accurate time and date

--- javasc/index.js
+++ javasc/index.js
@@ -1,1 +1,22 @@
+function updateTime() {
+  let johannesburgElement = document.querySelector("#johannesburg");
+  let johannesburgDateElement = johannesburgElement.querySelector(".date");
+  let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+  let johannesburgTime = moment().tz("Africa/Johannesburg");
 
+  johannesburgDateElement.innerHTML =
+    johannesburgTime.format(" MMMM D dddd  YYYY");
+  johannesburgTimeElement.innerHTML = johannesburgTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+  //Miami
+  let miamiElement = document.querySelector("#miami");
+  let miamiDateElement = miamiElement.querySelector(".date");
+  let miamiTimeElement = miamiElement.querySelector(".time");
+  let miamiTime = moment().tz("America/New_York");
+
+  miamiDateElement.innerHTML = miamiTime.format(" MMMM D ddd YYYY");
+  miamiTimeElement.innerHTML = miamiTime.format("h:mm:ss [<small>]A[</small>]");
+}
+setInterval(updateTime, 1000);
+updateTime();
